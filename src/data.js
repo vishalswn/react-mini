5a0e3a2a9ce16fe6df08005701ede711c54c1c8e
vishal <vishal.thoughtwin@gmail.com>
2021-10-27 18:45:44 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://pbs.twimg.com/profile_images/3563274471/089d696f134d7eb4c90dc8e6d04dd044.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://pbs.twimg.com/profile_images/3563274471/089d696f134d7eb4c90dc8e6d04dd044.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://pbs.twimg.com/profile_images/3563274471/089d696f134d7eb4c90dc8e6d04dd044.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://pbs.twimg.com/profile_images/3563274471/089d696f134d7eb4c90dc8e6d04dd044.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://pbs.twimg.com/profile_images/3563274471/089d696f134d7eb4c90dc8e6d04dd044.jpeg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://pbs.twimg.com/profile_images/3563274471/089d696f134d7eb4c90dc8e6d04dd044.jpeg",
+      link: "http://lama.dev",
+    },
+  ];
